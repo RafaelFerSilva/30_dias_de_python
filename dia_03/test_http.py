309071b9e310571bc02960df4453d400b28d9afe
Rafael Silva <todosomtodavoz@gmail.com>
2024-11-03 11:12:03 -0300

    Dia 03 - Testando requisições HTTP com HTTPX e RESPX

---
+++ dia_03/test_http.py
@@ -0,0 +1,70 @@
+import httpx
+import respx
+from  typing import Literal, TypeAlias, get_args
+
+URL_COTACAO = 'https://economia.awesomeapi.com.br/json/last/{}'
+Moeda: TypeAlias = Literal['EUR', 'USD', 'BTC']
+
+def cotacao(moeda: Moeda):
+  code = f'{moeda}-BRL'
+  try:
+    response = httpx.get(URL_COTACAO.format(code))
+    data = response.json()[code.replace('-', '')]
+
+    return f'Última cotação: {data['high']}'
+  except KeyError:
+    return f'Código de moeda inválido. Use {get_args(Moeda)}'
+  except httpx.InvalidURL:
+    return f'Código de moeda inválido. Use {get_args(Moeda)}'
+  except httpx.ConnectError:
+    return 'Erro de conexão, tente mais tarde'
+  except httpx.TimeoutException:
+    return 'Erro de conexão, tente mais tarde'
+
+@respx.mock
+def test_dolar():
+  # Arange
+  mocked_response = httpx.Response(200, json={'USDBRL': {'high': 5.8747}})
+  respx.get(URL_COTACAO.format('USD-BRL')).mock(mocked_response)
+
+  # Act
+  result = cotacao('USD')
+
+  # Assert
+  assert result == 'Última cotação: 5.8747'
+
+@respx.mock
+def test_moeda_errada():
+  # Arange
+  mocked_response = httpx.Response(200, json={})
+  respx.get(URL_COTACAO.format('MDT-BRL')).mock(mocked_response)
+
+  # Act
+  result = cotacao('MDT')
+
+  # Assert
+  assert (result == "Código de moeda inválido. Use ('EUR', 'USD', 'BTC')")
+
+def test_moeda_errada_erro_URL():
+  result = cotacao('\x11')
+  assert (result == "Código de moeda inválido. Use ('EUR', 'USD', 'BTC')")
+
+def test_erro_conexao(respx_mock):
+  # Arange
+  respx_mock.get(URL_COTACAO.format('USD-BRL')).mock(side_effect=httpx.ConnectError)
+
+  # Act
+  result = cotacao('USD')
+
+  # Assert
+  assert result == 'Erro de conexão, tente mais tarde'
+
+def test_erro_timeout(respx_mock):
+  # Arange
+  respx_mock.get(URL_COTACAO.format('USD-BRL')).mock(side_effect=httpx.TimeoutException)
+
+  # Act
+  result = cotacao('USD')
+
+  # Assert
+  assert result == 'Erro de conexão, tente mais tarde'
